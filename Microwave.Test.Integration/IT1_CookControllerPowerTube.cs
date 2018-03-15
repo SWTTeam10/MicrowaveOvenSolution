37b94eefc260ecd69520ffd18c95b0ad379929c0
StineSig <33084964+StineSig@users.noreply.github.com>
2018-03-15 15:23:07 +0100

    Integrationstest

---
+++ Microwave.Test.Integration/IT1_CookControllerPowerTube.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MicrowaveOvenClasses.Boundary;
+using MicrowaveOvenClasses.Controllers;
+using MicrowaveOvenClasses.Interfaces;
+using NSubstitute;
+using NUnit.Framework;
+
+
+namespace Microwave.Test.Integration
+{
+  
+    [TestFixture]
+    public class IT1_CookControllerPowerTube
+    {
+        private IOutput _output;
+        private ITimer _timer;
+        private IPowerTube _powerTube;
+        private IDisplay _display;
+        private ICookController _uut;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _output = Substitute.For<IOutput>();
+            _timer = Substitute.For<ITimer>();
+            _powerTube = new PowerTube(_output);
+            _display = Substitute.For<IDisplay>();
+            _uut = new CookController(_timer, _display, _powerTube);
+        }
+
+
+        [TestCase(60,10)]
+        public void Start_Start_OutlineTurnedOff(int power, int time)
+        {
+            _uut.StartCooking(power, time);
+
+            _output.Received().OutputLine($"PowerTube works with {power} %");
+        }
+
+        [Test]
+        public void Stop_StartAndStop_OutlineTurnedOff()
+        {
+            _uut.StartCooking(60,10);
+            _uut.Stop();
+          
+            _output.Received().OutputLine($"PowerTube turned off");        
+        }
+
+        [Test]
+        public void Stop_NoStart_DidNotRecivedOutline()
+        {
+            _uut.Stop();
+            _output.DidNotReceive().OutputLine($"PowerTube turned off");
+        }
+
+    }
+}
---
+++ Microwave.Test.Integration/IT2_CookController_PowerTube_Display.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MicrowaveOvenClasses.Boundary;
+using MicrowaveOvenClasses.Controllers;
+using MicrowaveOvenClasses.Interfaces;
+using NSubstitute;
+using NUnit.Framework;
+
+namespace Microwave.Test.Integration
+{
+    [TestFixture]
+    class IT2_CookController_PowerTube_Display
+    {
+        private IOutput _output;
+        private ITimer _timer;
+        private IPowerTube _powerTube;
+        private IDisplay _display;
+        private ICookController _uut;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _output = Substitute.For<IOutput>();
+            _timer = Substitute.For<ITimer>();
+            _powerTube = new PowerTube(_output);
+            _display = new Display(_output);
+            _uut = new CookController(_timer, _display, _powerTube);
+        }
+
+        [Test]
+        public void XXXX_XXXXX_OutlineTurnedXXXX()
+        {
+            _uut.StartCooking(65, 100);
+            _timer.TimeRemaining.Returns(100); 
+            int min = _timer.TimeRemaining/60;
+            int sec = _timer.TimeRemaining % 60; 
+            _display.ShowTime(min,sec);
+            _output.Received().OutputLine($"Display shows: {min:D2}:{sec:D2}");
+        }
+
+    }
+}
